c7c2e6f977269c56fbea2f7df268879674fa2523
arti2011 <artihsalunkhe@gmail.com>
2022-12-09 13:48:30 +0530

    variable declaretion and initilization
    variable declaretion and initilization

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+   var myName;
+   myName ="Arti Salunkhe";
+   console.log("myName");
+   var myFrdName ="shital dhotare";
+   console.log(myFrdName );
+
+   myFrdName="supriya Mali";
+   console.log("myFrdName");
---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName="Salunkhe Arti";
+console.log(myFullName);
+
+myFullName="Salunkhe Haridas arti";
+console.log(myFullName);
+
+var myLove="I Love JS";
+console.log(myLove);
+
+var firstName="Arti";
+var lastName="Salunkhe";
+var age="23";
+var address="phandharpur";
+var pin_code="413304";
+var gendar="female";
+var is_married="no";
+
+console.log(firstName,lastName,age,address,pin_code,is_married);
