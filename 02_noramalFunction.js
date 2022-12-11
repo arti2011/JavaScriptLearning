9215fc0ab9953200b0a1bd7ded749a21db7c1e32
arti2011 <artihsalunkhe@gmail.com>
2022-12-11 15:14:32 +0530

    normal function with bank details
    normal function with bank details

---
+++ 02_noramalFunction.js
@@ -0,0 +1,6 @@
+function bankDetails(){
+console.log("Here are the Bank Details:Bank Name:CITI Bank,Account Number:3456782345,Location: pune,Pincode:431202"); console.log("Here are the Bank Details:Bank Name:Axis Bank,Account Number:7856782345,Location:Mumbai,Pincode 441202");
+console.log("Here are the Bank Details:Bank Name:HDFC Bank,Account Number:8956782345,Location:Pune,Pincode:631202")
+
+}
+bankDetails();
