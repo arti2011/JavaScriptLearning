9df443a153fd2e6be0b160c67aff4cc87162c0a4
arti2011 <artihsalunkhe@gmail.com>
2022-12-17 08:32:08 +0530

    arithmetic operator
    arithmetic operator

---
+++ 04_aritmeticOperator.js
@@ -0,0 +1,40 @@
+var num1=10;
+var num2=5;
+var addResult=num1+num2;
+console.log(`Addition is${addResult}`);
+console.log(`subtraction is${num1-num2}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`=================Assignment Oprrator==============================`);
+var num1=10;
+var num2=5;
+ num1+=num2;
+console.log(`compound addition ${num1}`);
+
+
+
+
+
+console.log(`compound substraction ${num1-=num2}`);
+
+
+
+
+
+
+
+
+
+
