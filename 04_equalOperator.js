a9b80beef5a810b952943d95a5adfdd55a5e2bdf
arti2011 <artihsalunkhe@gmail.com>
2022-12-17 08:30:52 +0530

    equal operators
    equal operators

---
+++ 04_equalOperator.js
@@ -0,0 +1,16 @@
+console.log("==================comparison operator==========================");
+console.log(` (0 ==' ')  is`,true);
+
+console.log(` (0=='0')  is`, true);
+
+console.log(` (0==False) is`, true);
+
+console.log(` (null == undefined)  is`, true);
+
+console.log(` (1 ==[1])  is`, true);
+
+console.log(` (1 == true)  is`, true);
+
+console.log(` (1 == '1')  is`, true);
+
+
