7eaf931c4b2dc27ca13bad3689488ef062f11f6c
arti2011 <artihsalunkhe@gmail.com>
2022-12-18 21:32:32 +0530

    check out the few interesting fact
    check out the few interesting fact

---
+++ 05_ifBlock.js
@@ -0,0 +1,31 @@
+console.log(`=================numeric string used with + gives string type====================`);
+var result;
+result=`3`+2 ;
+console.log(result);
+
+var result;
+result=`3`+true ;
+console.log(result);
+
+var result;
+result=`3`+undefined ;
+console.log(result);
+
+var result;
+result=`3`+null ;
+console.log(result);
+console.log("==================boolean conversion to number=================================================");
+var result
+result=`4`-true;
+console.log(result);
+
+result= 4+true;
+console.log(result);
+
+result=`4`-false;
+console.log(result);
+console.log("=======undefined used with number,boolean or null given NaN=====================================")
+var result;
+result= 4+ undefined;
+console.log(result);
+
