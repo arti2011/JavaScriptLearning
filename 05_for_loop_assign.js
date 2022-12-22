93d1242aff0e2e8ff64b75386cc72274797f6cf2
arti2011 <artihsalunkhe@gmail.com>
2022-12-22 21:21:57 +0530

    forLoop to print the even & add no or table
    forLoop to print the even & add no or table

---
+++ 05_for_loop_assign.js
@@ -0,0 +1,42 @@
+// WAP to print number from 5 to 15 by incrimenting 1
+console.log("==========================1.===============================");
+for (let index=5; index < 15; index++) {
+    console.log(index);
+    
+}
+//WAP to print number from 50 to 40 by decrementing  1
+console.log("==========================2.==================================");
+for (let index = 50; index >=40 ; index=index-1) {
+    console.log(index);
+    
+}
+// WAP to find first 15 odd numbers
+console.log("============================3.===============================");
+for (let index = 1; index <=15; index+2) {
+    console.log(index);
+    
+}
+// WAP to find 10 even numbers
+console.log("============================4===================================");
+for (let index = 2; index <=15; index+2) {
+    console.log(index);
+    
+}
+//WAP to print table of like --> 5 10 15 20 25 .....50
+console.log("=============================5====================================="); 
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+    
+}
+//WAP to print table of in reverse ordar like --> 100 90 80 70......
+console.log("===============================6.========================================");
+for (let index =100; index >=10; index=index-1) {
+    console.log(index);
+    
+}
+//WAP to print table 0 like 10 20 30 40 .....100
+console.log("===============================6.========================================");
+for (let index =10; index <=100; index=index+1) {
+    console.log(index);
+}
+    
