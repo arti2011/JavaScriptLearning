024a260059a0097c0ab9eb28e83d0e054958a36c
arti2011 <artihsalunkhe@gmail.com>
2022-12-27 22:45:32 +0530

    object with data member and function
    object with data member and function

---
+++ o7_object_assinment2.js
@@ -0,0 +1,69 @@
+
+ 
+
+function showDetails() {
+    const sbiBank = {
+        bankName : "State Bank Of India",
+        location : "Phandharpur",
+        accountNo : 6251286785663,
+        IFSC : "SBIN0000335",
+        interestRate : "7%",
+        add : function showDetails(){
+            console.log(`Please visit again always welcome`);
+        }
+    }
+    console.log(`---------- SBI Bank ----------`);
+    console.log(sbiBank);
+    }
+    showDetails();
+    console.log(``);
+    
+    function showDetails1(){
+    const axisBank = {
+        bankName : "Axis Bank",
+        location : "Pandharpur",
+        accountNo : 354233665588,
+        IFSC : "AXIS00556",
+        interestRate : "5%",
+        add : function showDetails(){
+            console.log(`Please visit again always welcome`);
+        }
+    }
+    console.log(`---------- Axis Bank ----------`);
+    console.log(axisBank);
+    }
+    showDetails1();
+    console.log(``);
+    
+    function showDetails2(){
+        const hdfcBank = {
+            bankName : "HDFC Bank",
+            location : "Pune",
+            accountNo : 649733665588,
+            IFSC : "HDFC0000935",
+            interestRate : "8%",
+            add : function showDetails(){
+                console.log(`Please visit again always welcome`);
+            }
+        }
+        console.log(`---------- HDFC Bank ----------`);
+        console.log(hdfcBank);
+        }
+        showDetails2();
+        console.log(``);
+    
+        function showDetails3(){
+            const yesBank = {
+                bankName : "YES Bank",
+                location : "Mumbai",
+                accountNo : 554544555335,
+                IFSC : "YB0000495",
+                interestRate : "9%",
+                add : function showDetails(){
+                    console.log(`Please visit again always welcome`);
+                }
+            }
+            console.log(`---------- YES Bank ----------`);
+            console.log(yesBank);
+            }
+            showDetails3();
