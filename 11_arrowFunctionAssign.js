feed7edee3dd700780f091b915d55b09e3ab4c76
arti2011 <artihsalunkhe@gmail.com>
2023-01-18 08:50:17 +0530

    arrow function and array.js
    arrow function and array.js

---
+++ 11_arrowFunctionAssign.js
@@ -0,0 +1,31 @@
+console.log(`---------------------------------------------------------------------------------------`);
+console.log(`Simple message with array function`);
+let show = ()=>{
+    console.log("Good Morning,Today is Monday");
+}
+show();
+console.log(`-------------------------------------------------------------------------------------------`);
+console.log(`Multipilcation of  three numbers & multiplication three members with one value `);
+let multiply = (n1, n2,n3)=> {
+  console.log(n1*n2*n3);  
+    
+}
+ multiply(5, 5, 2);
+
+ let mul = (n4,n5,n6)=> {
+    console.log(n4*n5*n6);
+ }
+ mul(10,4,1)
+ console.log(`----------------------------------------------------------------------------------------`);
+ console.log(`addtion of 5 parametar and return it`);
+ let addtion = (n1, n2,n3,n4,n5)=>{
+    let add=(n1+n2+n3+n4+n5);
+    return add
+}
+let addResult = addtion(100,100,200,349,756);
+console.log(addResult);
+console.log(``);
+
+
+
+
